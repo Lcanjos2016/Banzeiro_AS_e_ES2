360abc9b95658ab082a5c805a0a964b884108847
IAGOx46 <iagovinicius213@gmail.com>
2025-11-26 19:44:17 -0400

    att

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyA_s9aU_u4y9-QdKAJaTve8md9FZxc5CS8",
+  authDomain: "banzeiro-5f94c.firebaseapp.com",
+  projectId: "banzeiro-5f94c",
+  storageBucket: "banzeiro-5f94c.firebasestorage.app",
+  messagingSenderId: "485805652054",
+  appId: "1:485805652054:web:fe648f81b3bdf94ceca3f7"
 };
 
 const app = initializeApp(firebaseConfig);
